e9a1fa07ce0d531e8c1c89c51de96921f9ea4023
ShadowX117 <malexander@aussiebush.net>
2026-06-13 22:53:24 +1000

    Update Hellbomb Script.ps1
    Added a note about PII

--- Hellbomb Script.ps1
+++ Hellbomb Script.ps1
@@ -2757,6 +2757,7 @@ Function RunAndPause
     & $ScriptBlock
     Write-Host "`n--- Paused ---"
     Write-Host "Copy any results you want to save, then press [SPACEBAR] to return to the menu."
+	Write-Host "Note: The Hellbomb Script output does not include any Sensitive or Personally Identifiable Information and is safe to share"
     Pause
 }
 Function Invoke-HD2StatusChecks {
